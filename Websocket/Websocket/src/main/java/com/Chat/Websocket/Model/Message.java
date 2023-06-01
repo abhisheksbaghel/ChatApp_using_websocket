3e93a9dbf579ece5175cfd41a9d7da9804dcf27c
Abhishek Singh Baghel <92367280+abhisheksbaghel@users.noreply.github.com>
2023-06-01 15:48:32 +0530

    Chat app with basic UI

---
+++ Websocket/Websocket/src/main/java/com/Chat/Websocket/Model/Message.java
@@ -0,0 +1,15 @@
+package com.Chat.Websocket.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String name;
+    private String content;
+
+}
